21fb997d7361de5d23401dcc3b415fd1bb120476
Srinivasu Gannem <srinivasu.gannem@nxp.com>
2021-07-14 15:46:18 +0530

    added svg icon in Mid Area

--- src/components/MidArea.js
+++ src/components/MidArea.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Icon from "./Icon";
 
 export default function MidArea() {
-  return <div className="flex-1 h-full overflow-auto">{"mid area"} </div>;
+  return <div className="flex-1 h-full overflow-auto">
+    {"mid area"} 
+    <Icon name="brain" size={55} className="text-green-600 mx-2" />
+    </div>;
 }
